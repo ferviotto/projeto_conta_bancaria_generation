b29dde2bd82c292759ae565c401389dda368f330
Fernanda Viotto De Gobbi <164960590+ferviotto@users.noreply.github.com>
2024-06-14 16:52:21 -0300

    Update Menu.java

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,7 +1,6 @@
 package conta;
 
 import java.util.Scanner;
-
 import conta.model.ModelConta;
 import conta.util.cores;
 
@@ -11,15 +10,19 @@ public class Menu {
 		// TODO Auto-generated method stub
 		
 		//Teste da Classe Conta
-		 ModelConta c1 = new ModelConta(13694, 1620, "Fernanda");
-		 ModelConta c2 = new ModelConta(13695, 1621,"Giuliano");
-		
+		 ModelConta c1 = new ModelConta(1, 123, 1, "Fernanda",10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+
 		Scanner read = new Scanner(System.in);
-		
+
 		int op;
 				
 		while(true) {
-			
+			System.out.println(Cores.TEXT_BLUE_BRIGHT + Cores.ANSI_BLACK_BACKGROUND
 			System.out.println("\n \tBanco da Fer");
 			System.out.println("\n1 - Criar Conta");
 			System.out.println("\n2 - Listar todas as contas");
@@ -30,7 +33,8 @@ public class Menu {
 			System.out.println("\n7 - Depositar");
 			System.out.println("\n8 - Transferir valores entre contas");
 			System.out.println("\n9 - Sair");
-
+			
+		op = read.nextInt();
 			
 			System.out.println("\nEntre com a opção desejada: ");
 			op = read.nextInt();
@@ -46,39 +50,31 @@ public class Menu {
 			switch (op) {
 			case 1:
 				System.out.println(cores.TEXT_PURPLE + "\nCriar conta");
-				c1.criarConta();
 				break;
 			
 			case 2:
 				System.out.println(cores.TEXT_GREEN + "\nListar todas as contas");
-				c1.listarConta();
 				break;
 				
 			case 3:
 				System.out.println(cores.TEXT_YELLOW + "\nConsultar dados da conta por número");
-				c1.buscarConta();
-                break;
+               		 	break;
 			case 4:
 				System.out.println(cores.TEXT_BLUE + "\nAtualizar dados da conta");
-				c1.atualizarConta();
 				break;
 			case 5:
 				System.out.println(cores.TEXT_PURPLE_BRIGHT + "\nApagar a conta");
-				c1.apagarConta();
 				break;
 			case 6:
 				System.out.println(cores.TEXT_GREEN_BOLD + "\nSaque");
-				c1.sacar();
-               	break;
+              		 	break;
 			case 7:
 				System.out.println(cores.TEXT_YELLOW_BOLD + "\nDepósito");
-				c1.depositar();
-                break;
+                		break;
 			case 8:
 				System.out.println(cores.TEXT_BLUE_BRIGHT + "\nTransferência entre contas");
-				c1.transferir(c2);
-                break;
-			default:
+                		break;
+				default:
 				System.out.println(cores.TEXT_RED_BOLD + "\nOpção Inválida. Por favor, digite uma opção válida." + cores.TEXT_RESET);
                 		break;
 			}
